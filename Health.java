9c35ea25e95e6878c4bff03ce1246b4f11b4a4e9
rod2016 <roderickjackson@Rodericks-MacBook-Pro.local>
2014-11-10 21:47:04 -0500

    Prelim coding
    added basic health feature and established items in rooms. Can have
    multiple items in a room and must iterate through set ( not yet
    implemented)

---
+++ Health.java
@@ -0,0 +1,67 @@
+
+/**
+ * Write a description of class Health here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Health
+{
+    // instance variables - replace the example below with your own
+    private int healthNum;
+    private String healthString;
+
+    /**
+     * Constructor for objects of class Health
+     */
+    public Health()
+    {
+        healthNum = 10;
+        healthString = getHealthString(healthNum);
+        
+    }
+
+    public String getHealthString(int healthNum)
+    {
+        switch(healthNum) {
+            case 1:
+            healthString = "#         ";
+            break;
+            case 2:
+            healthString = "##        ";
+            break;
+            case 3:
+            healthString = "###       ";
+            break;
+            case 4:
+            healthString = "####      ";
+            break;
+            case 5:
+            healthString = "#####     ";
+            break;
+            case 6:
+            healthString = "######    ";
+            break;
+            case 7:
+            healthString = "#######   ";
+            break;
+            case 8:
+            healthString = "########  ";
+            break;
+            case 9:
+            healthString = "######### ";
+            break;
+            case 10:
+            healthString = "##########";
+            break;
+        }
+        return healthString = healthString;
+    }
+    
+    public int getHealthNum()
+    {
+        return healthNum;
+        
+    }
+    
+}
