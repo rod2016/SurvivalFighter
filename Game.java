148a3b2c5ae43531f9f9b28370d5c89ce8a5f418
rod2016 <roderickjackson@s172-20-219-h224.paws.uga.edu>
2014-11-12 16:08:06 -0500

    ###### revert point######

--- Game.java
+++ Game.java
@@ -24,8 +24,8 @@ public class Game
     private Room currentRoom;
     private Health health;
     private int currentHealthNum;
-    private ArrayList itemsInPack;
-    private ArrayList boatItems;
+     private ArrayList<String> itemsInPack;
+    private ArrayList<String> boatItems;
     /**
      * Create the game and initialise its internal map.
      */
@@ -37,7 +37,7 @@ public class Game
         parserWithFileInput = new ParserWithFileInput();
         health = new Health();
         currentHealthNum = health.getHealthNum();
-        itemsInPack = new ArrayList();
+        itemsInPack = new ArrayList<String>();
     }
 
     /**
@@ -108,7 +108,15 @@ public class Game
         while (! finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
+            if(currentHealthNum <= 0){
+                finished = true;
+            } 
         }
+        if(currentHealthNum <= 0)
+        {
+             System.out.println("You died");
+             
+            }
         System.out.println("Thank you for playing.  Good bye.");
     }
     public void playWithFileInput() 
@@ -136,6 +144,7 @@ public class Game
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
         System.out.println(health.getHealthString(currentHealthNum));
+        System.out.println("your pack contains: " +printItemsInPack());
     }
 
     /**
@@ -161,6 +170,9 @@ public class Game
         }
         else if (commandWord.equals("get")) {
            pickUpItem(command);
+        }
+        else if (commandWord.equals("die")) {
+           die();
         }
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
@@ -208,7 +220,7 @@ public class Game
         else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
-            
+            System.out.println("your pack contains: " +printItemsInPack());
             System.out.println(health.getHealthString(currentHealthNum));
         }
     }
@@ -229,10 +241,8 @@ public class Game
         }
         else {
             
-            //currentRoom = nextRoom;//
-           System.out.println("add to add to pack");
-            
-            //System.out.println(health.getHealthString(currentHealthNum));
+            itemsInPack.add(item);
+            System.out.println("you picked up the "+item);
         }
         
         
@@ -251,5 +261,29 @@ public class Game
         else {
             return true;  // signal that we want to quit
         }
+    }
+      
+    public String printItemsInPack()
+    {
+        int i = 1;
+        int numberOfItemsInPack = itemsInPack.size();
+        String itemsInPackList = new String();
+        if (itemsInPack.size() == 0){
+            return "you have nothing in you bag";
+        }
+        
+        while(i <= itemsInPack.size())
+        {
+            
+            itemsInPackList = itemsInPackList +" "+ itemsInPack.get(i-1);
+            i++;
+        }
+        return itemsInPackList;
+        
+    }
+    public void die()
+    {
+       currentHealthNum = currentHealthNum - 1;
+       System.out.println("Hurt by 1");
     }
 }
